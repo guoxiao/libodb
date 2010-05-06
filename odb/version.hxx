778fd5b528a771deec035dafd7f16068c7e132eb
Boris Kolpackov <boris@codesynthesis.com>
2010-05-06 16:47:52 +0200

    Add version files

---
+++ odb/version.hxx
@@ -0,0 +1,36 @@
+// file      : odb/version.hxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : Copyright (c) 2005-2010 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef ODB_VERSION_HXX
+#define ODB_VERSION_HXX
+
+// Version format is AABBCCDD where
+//
+// AA - major version number
+// BB - minor version number
+// CC - bugfix version number
+// DD - alpha / beta (DD + 50) version number
+//
+// When DD is not 00, 1 is subtracted from AABBCC. For example:
+//
+// Version     AABBCCDD
+// 2.0.0       02000000
+// 2.1.0       02010000
+// 2.1.1       02010100
+// 2.2.0.a1    02019901
+// 3.0.0.b2    02999952
+//
+
+// ODB interface version: minor, major, and alpha/beta versions.
+//
+#define ODB_VERSION     9901
+#define ODB_VERSION_STR "1.0.a1"
+
+// libodb version: interface version plus the bugfix version.
+//
+#define LIBODB_VERSION     999901
+#define LIBODB_VERSION_STR "1.0.0.a1"
+
+#endif // ODB_VERSION_HXX
