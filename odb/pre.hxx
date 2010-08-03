c5505f031613e0cdd6c99d24379f5316e536acaa
Boris Kolpackov <boris@codesynthesis.com>
2010-08-03 14:06:02 +0200

    Add pre/post header for compiler-specific actions

---
+++ odb/pre.hxx
@@ -0,0 +1,8 @@
+// file      : odb/pre.hxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : Copyright (c) 2009-2010 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifdef _MSC_VER
+#  include <odb/compilers/vc/pre.hxx>
+#endif
