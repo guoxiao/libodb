591bcad6b05c1a835d4b721ecb143ff7ef5e7f34
Boris Kolpackov <boris@codesynthesis.com>
2010-09-09 10:19:52 +0200

    Add empty source files to allow inline function exporting

---
+++ odb/details/mutex.cxx
@@ -0,0 +1,15 @@
+// file      : odb/details/mutex.cxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : Copyright (c) 2009-2010 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#include <odb/details/mutex.hxx>
+
+namespace odb
+{
+  namespace details
+  {
+    // This otherwise unnecessary file is here to allow instantiation
+    // of inline functions for exporting.
+  }
+}
