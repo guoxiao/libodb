43e592013b1dc786cd224ce827cad24f1805ca49
Boris Kolpackov <boris@codesynthesis.com>
2010-11-17 18:05:06 +0200

    Add support for unidirectional object relationships
    New test: common/relationship.

---
+++ odb/details/unused.hxx
@@ -0,0 +1,15 @@
+// file      : odb/details/unused.hxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : Copyright (c) 2009-2010 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef ODB_UNUSED_DETAILS_HXX
+#define ODB_UNUSED_DETAILS_HXX
+
+#include <odb/pre.hxx>
+
+#define ODB_POTENTIALLY_UNUSED(x) (void)x
+
+#include <odb/post.hxx>
+
+#endif // ODB_UNUSED_DETAILS_HXX
