3674a82b9c09757b479e485a673a7a556f12284b
Boris Kolpackov <boris@codesynthesis.com>
2011-01-24 14:28:08 +0200

    Add dummy source file for DLL export

---
+++ odb/lazy-ptr-impl.cxx
@@ -0,0 +1,12 @@
+// file      : odb/lazy-ptr-impl.cxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#include <odb/lazy-ptr-impl.hxx>
+
+namespace odb
+{
+  // This otherwise unnecessary file is here to allow instantiation
+  // of inline functions for exporting.
+}
