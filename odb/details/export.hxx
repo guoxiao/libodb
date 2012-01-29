a73b35b1944a1dc2f5564f6aa2087af090c87e05
Boris Kolpackov <boris@codesynthesis.com>
2012-01-29 14:25:54 +0200

    Remove author field from file header
    Too much effort to maintain.

--- odb/details/export.hxx
+++ odb/details/export.hxx
@@ -1,5 +1,4 @@
 // file      : odb/details/export.hxx
-// author    : Boris Kolpackov <boris@codesynthesis.com>
 // copyright : Copyright (c) 2005-2011 Code Synthesis Tools CC
 // license   : GNU GPL v2; see accompanying LICENSE file
 
