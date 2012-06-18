dcd9226bedc5afb97193d3b62ea47434c0d8e47d
Boris Kolpackov <boris@codesynthesis.com>
2012-06-18 09:17:14 +0200

    Bump version to 2.0.1

--- odb/version.hxx
+++ odb/version.hxx
@@ -31,8 +31,8 @@
 
 // libodb version: interface version plus the bugfix version.
 //
-#define LIBODB_VERSION     2000000
-#define LIBODB_VERSION_STR "2.0.0"
+#define LIBODB_VERSION     2000100
+#define LIBODB_VERSION_STR "2.0.1"
 
 #include <odb/post.hxx>
 
